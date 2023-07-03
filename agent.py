07c718bd9fca0a6061fab53e21abb3146e72ae37
Sylvain <sylvainhu1999@gmail.com>
2023-07-03 16:27:07 +0100

    Add functions to link agents V1

--- agent.py
+++ agent.py
@@ -12,18 +12,21 @@ class Players(Agent):
         self.reputation = reputation
         self.skill = skill
         self.value = value
+        self.agent = []
 
+    # Set the players' value
     def set_value(self):
-        self.value = self.reputation * self.skill
+        self.value = self.reputation * self.skill / self.age
 
-    
+    # Link players with agents
+    def add_agent(self, F_agents):
+        self.agent.append(F_agents)
+
 
     def step(self):
         print("Player made!")
         print("Hi, I am player " + str(self.unique_id) + ". My age is " + str(self.age) + ". My contract is " + self.contract + ". My rep is " + str(self.reputation) + ". My skill is " + str(self.skill) 
               + ". My value is " + str(self.value) + ".")
-        
-
 
 class Club(Agent):
     """An agent with fixed initial wealth."""
@@ -33,13 +36,17 @@ class Club(Agent):
         self.spending = spending
         self.objectives = objectives
         self.fans = fans
+        self.team = []
+
+    def add_player(self, player):
+        self.team.append(player)
     
     def step(self):
         print("Club made!")
 
 class F_Agents(Agent):
     """An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model, cut, network, n_skills, clients):
+    def __init__(self, unique_id, model, cut, network, n_skills):
         super().__init__(unique_id, model)
         self.cut = cut
         self.network = network
