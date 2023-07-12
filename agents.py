14df0323b8e0ab6a9e5c4fbcee60d48c657a8b9e
Sylvain <sylvainhu1999@gmail.com>
2023-07-12 20:00:52 +0100

    Further work and cleanup for model and agents

--- agents.py
+++ agents.py
@@ -1,25 +1,63 @@
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
+import random
+
+
+def set_club_type(club, type):
+    """Updates club's attributes according to its type:
+    1 = Big, 2 = Medium, 3 = Small
+
+    Args:
+        Club: The agent instance.
+        type (int): The type of club.
+    """
+    if type == 1:
+        club.objectives = random.randint(10, 1000)
+        club.fans = random.randint(700, 1000)
+    if type == 2:
+        club.objectives = random.randint(10, 1000)
+        club.fans = random.randint(300, 700)
+    if type == 3:
+        club.objectives = random.randint(10, 1000)
+        club.fans = random.randint(10, 300)
+
 
 class Club(Agent):
-    """An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model, revenue, spending, objectives, fans):
+    def __init__(self, unique_id, model, revenue, spending, type):
         super().__init__(unique_id, model)
         self.revenue = revenue
         self.spending = spending
-        self.objectives = objectives
-        self.fans = fans
+        self.type = type
+        self.objectives = 0
+        self.fans = 0
         self.team = []
+        set_club_type(self, type)
 
     def add_player(self, player):
         self.team.append(player)
+
+    def set_revenue(self):
+        self.revenue = 700 * self.fans
+
+    def team_level(self):
+        if len(self.team) > 0:
+            total = sum(player.skill for player in self.team)
+            average = round(total / len(self.team), 2)
+            return average
+        else:
+            print("No players in the team yet")
+
+    def wins(self):
+        self.fans *= 1.3
     
     def step(self):
-        print("Club made!" + str(self.unique_id))
+        squad_id = [str(player.unique_id) for player in self.team]
+        squad_list = ', '.join(squad_id)
+        print("Club made!" + str(self.unique_id) + ". My type is: " + str(self.type) + ". My team has player number: " + squad_list + ". The average level is: " + 
+              str(self.team_level()) + ". The revenue is " + str(self.revenue) + ".")
 
 class F_Agents(Agent):
-    """An agent with fixed initial wealth."""
     def __init__(self, unique_id, model, cut, network, n_skills):
         super().__init__(unique_id, model)
         self.cut = cut
@@ -31,7 +69,9 @@ class F_Agents(Agent):
         self.clients.append(player)
 
     def step(self):
-        print("Agent made!" + str(self.unique_id) + ".")
+        client_id = [str(player.unique_id) for player in self.clients]
+        client_list = ', '.join(client_id)
+        print("Agent made!" + str(self.unique_id) + " My clients are: " + client_list + ".")
 
 class Players(Agent):
     """An agent with fixed initial wealth."""
@@ -58,6 +98,9 @@ class Players(Agent):
         self.club = club
         club.add_player(self)
 
+    def higher_rep(self):
+        self.reputation += 5
+
     # Player ages
     def ageing(self):
         self.age += 1
@@ -68,7 +111,7 @@ class Players(Agent):
               str(self.reputation) + ". My skill is " + str(self.skill) + ". My value is " + str(self.value) + " millions.")
         
         if self.F_agent is not None:
-            print("My agent is agent " + str(self.F_agent.unique_id))
+            print("My agent is agent " + str(self.F_agent.unique_id) + ". My club is club " + str(self.club.unique_id))
         
         # Player is one year older each 10 steps
         if self.model.schedule.steps != 0 and self.model.schedule.steps % 10 == 0:
--- model.py
+++ model.py
@@ -10,65 +10,96 @@ class MyModel(mesa.Model):
     """A model with some number of agents."""
 
     def __init__(self, C, F, P, width, height):
-        # self.num_agents = N
+        self.num_clubs = C
+        self.num_agents = F
+        self.num_players = P
         self.grid = MultiGrid(width, height, True)
         self.schedule = mesa.time.RandomActivationByType(self)
-        self.is_initial_step = True
-        self.kill_agents = []
+        # self.is_initial_step = True
+        self.clubs = []
+        self.pool = []
+        self.highest_unique_id = C + F
+        self.team_skill_levels = {}
+        self.second_team_skill_levels = {}
+        self.final_results = {}
 
-        # Create clubs
-        clubs = []
-        for i in range(C):
-            club = Club(i, self, revenue = 0, spending = 0, objectives = 0, fans = 0)
-            self.schedule.add(club)
-            clubs.append(club)
 
-            # Add the agent to a random grid cell
-            # x = self.random.randrange(self.grid.width)
-            # y = self.random.randrange(self.grid.height)
-            # self.grid.place_agent(f_agent, (x, y))
-
-        # Create agents
-        pool = []
-        for i in range(F):
-            f_agent = F_Agents(C + i, self, cut = random.randint(3, 10), network = "Signed", n_skills = random.randint(1, 10))
-            self.schedule.add(f_agent)
-            pool.append(f_agent)
-
-            # Add the agent to a random grid cell
-            # x = self.random.randrange(self.grid.width)
-            # y = self.random.randrange(self.grid.height)
-            # self.grid.place_agent(f_agent, (x, y))
-
-        # Create players
-        for i in range(P):
-            player = Players(C + F + i, self, age = random.randint(18, 40), contract = "Signed", reputation = random.randint(1, 10), 
-                             skill = random.randint(1, 10), value = 0)
-            player.set_value()
-            self.schedule.add(player)
-
-            # Link players to agents
-            choice = random.choice(pool)
-            player.link_agent(choice)
-
-            # Add the agent to a random grid cell
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(player, (x, y))
-
+        # Creating agents
+        self.create_clubs(C)
+        self.create_agents(F)
+        self.create_players(P)
 
     def step(self):
         """Advance the model by one step."""
         print("\nStep:", self.schedule.steps)
 
         # First step 
-        if self.is_initial_step:
-            print("First")
-            self.is_initial_step = False
+        # if self.is_initial_step:
+        #     print("First Step")
+        #     self.is_initial_step = False
+
+        if self.schedule.steps != 0 and self.schedule.steps %10 == 0:
+            print("Season over")
+            self.final_skill_levels()
+            self.print_final_skill_levels()
+            self.winner()
+
+        if self.schedule.steps != 0 and self.schedule.steps % 5 == 0:
+            print("Transfer market opens")
 
         self.schedule.step()
         self.retire_players()
+        player_count = self.count_players()
+        # print("Number of players: ", player_count)
 
+        # If a player retires, a new player will be created
+        if player_count < self.num_players:
+            self.create_players(self.num_players - player_count)
+
+        if self.schedule.steps != 1 and self.schedule.steps % 5 == 1:
+
+            print("Close")
+            self.average_skill_levels()
+            self.print_average_skill_levels()
+
+        # player_count = self.count_players()
+        # print("Number of players: ", player_count)
+
+
+
+    # Functions
+    def create_clubs(self, C):
+        for i in range(C):
+            club = Club(i, self, revenue = 0, spending = 0, type = random.randint(1, 3))
+            club.set_revenue()
+            self.schedule.add(club)
+            self.clubs.append(club)
+
+    def create_agents(self, F):
+        for i in range(F):
+            f_agent = F_Agents(self.num_clubs + i, self, cut = random.randint(3, 10), network = "Signed", n_skills = random.randint(1, 10))
+            self.schedule.add(f_agent)
+            self.pool.append(f_agent)
+
+    def create_players(self, P):
+        for i in range(P):
+            self.highest_unique_id += 1
+            player_id = self.highest_unique_id
+            player = Players(player_id, self, age = random.randint(18, 39), contract = "Signed", reputation = random.randint(1, 10), 
+                             skill = random.randint(1, 10), value = 0)
+            player.set_value()
+            self.schedule.add(player)
+
+            # Link players to agents
+            choice = random.choice(self.pool)
+            player.link_agent(choice)
+            choice = random.choice(self.clubs)
+            player.join_club(choice)
+
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(player, (x, y))
 
     # Players go to retirement when age >= 40
     def retire_players(self):
@@ -76,8 +107,51 @@ class MyModel(mesa.Model):
         if players_to_retire:
             for agent in players_to_retire:
                 print("Player " + str(agent.unique_id) + " retires!")
+
+                if agent.F_agent is not None:
+                    agent.F_agent.clients.remove(agent)
+
+                if agent.club is not None:
+                    agent.club.team.remove(agent)
+
                 self.schedule.remove(agent)
                 self.grid.remove_agent(agent)
         else:
             print("No players retire!")
 
+    # Count current players in the model
+    def count_players(self):
+        count = 0
+        for agent in self.schedule.agents:
+            if isinstance(agent, Players):
+                count += 1
+        return count
+
+    def average_skill_levels(self):
+        self.team_skill_levels = {}
+        for club in self.clubs:
+            team_skill = club.team_level()
+            self.team_skill_levels[club.unique_id] = team_skill
+        
+    def final_skill_levels(self):
+        self.second_team_skill_levels = {}
+        self.final_results = {}
+        for club in self.clubs:
+            team_skill = club.team_level()
+            self.second_team_skill_levels[club.unique_id] = team_skill
+
+        for key in self.team_skill_levels:
+            if key in self.second_team_skill_levels:
+                self.final_results[key] = (self.team_skill_levels[key] + self.second_team_skill_levels[key]) / 2
+
+    def print_average_skill_levels(self):
+        for club_id, team_skill in self.team_skill_levels.items():
+            print("Club " + str(club_id) + " - Average Team Skill Level: " + str(team_skill))
+
+    def print_final_skill_levels(self):
+        for club_id, team_skill in self.final_results.items():
+            print("Club " + str(club_id) + " - Average Team Skill Level: " + str(team_skill))
+
+    def winner(self):
+        max_key = max(self.final_results, key=lambda x: self.final_results[x])
+        print("The winner is club: ", max_key)
